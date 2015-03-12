772d87b857427ed8d5c202916fb3864372d1d1c1
Marcelo Emanoel B. Diniz <marceloemanoel@gmail.com>
2015-03-12 18:02:52 -0300

    Fix formatting

--- app/assets/javascripts/sb-admin-2.js
+++ app/assets/javascripts/sb-admin-2.js
@@ -28,8 +28,13 @@ $(function() {
 
     var url = window.location;
     var element = $('ul.nav a').filter(function() {
-        return this.href == url || url.href.indexOf(this.href) === 0;
-    }).addClass('active').parent().parent().addClass('in').parent();
+                                   return this.href == url || url.href.indexOf(this.href) === 0;
+                               })
+                               .addClass('active')
+                               .parent()
+                               .parent()
+                               .addClass('in')
+                               .parent();
     if (element.is('li')) {
         element.addClass('active');
     }
